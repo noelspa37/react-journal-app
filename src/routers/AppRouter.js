00cda0304e31fec3bf7980cccc88bfbfb0ea47ab
Noel Callapiña <noel.platzi.spa.37@gmail.com>
2021-06-26 22:07:58 -0500

    feat: agregar var Cheking y IsLoggedIn

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {useDispatch} from 'react-redux'
 import {firebase} from '../firebase/firestore-config';
 import {
@@ -15,16 +15,30 @@ import { login } from '../actions/auth';
 export const AppRouter = () => {
 
     const dispatch = useDispatch();
+    const [cheking, setCheking] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    
 
     useEffect(() => {
         
         firebase.auth().onAuthStateChanged((user) => {
             if(user?.id) {
                 dispatch( login( user.uid, user.displayName));
+                setIsLoggedIn(true)
+            }else {
+                setIsLoggedIn(false)
             }
+
+            setCheking(false);
         })
 
-    },[dispatch])
+    },[dispatch, setCheking])
+
+    if (cheking){
+        return (
+            <h1>Espere ...</h1>
+        )
+    }
 
     return (
         <Router>
