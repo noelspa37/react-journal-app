7735851e4d58c0eb0b0ef22518f3a9af5cb8d489
Noel Callapiña <noel.platzi.spa.37@gmail.com>
2021-06-27 13:04:19 -0500

    feat: creacion notesReducer

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 import { Sidebar } from './Sidebar';
 import { NoteScreen } from '../notes/NoteScreen';
-// import { NothingSelected } from './NothingSelected';
+import { NothingSelected } from './NothingSelected';
 
 
 export const JournalScreen = () => {
+
+    const {active} = useSelector( state => state.notes );
+
     return (
         <div className="journal__main-content">
             
@@ -12,9 +16,12 @@ export const JournalScreen = () => {
 
 
             <main>
-
-                {/* <NothingSelected /> */}
-                <NoteScreen />
+                {
+                    (active)
+                    ? (<NoteScreen />)
+                    : (<NothingSelected />)
+                }
+                
 
             </main>
 
