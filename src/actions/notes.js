80b2ab72c9c27f9af629389d9cf06f789ac28bc2
Noel Callapiña <noel.platzi.spa.37@gmail.com>
2021-06-27 13:39:57 -0500

    feat: habilitar nota activa en journal

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,4 +1,5 @@
 import { db } from "../firebase/firestore-config";
+import { types } from "../types/types";
 
 
 export const startNewNote = () => {
@@ -14,6 +15,16 @@ export const startNewNote = () => {
 
         const docRef = await db.collection(`${uid}/journal/notes`).add( newNote);
 
-        console.log(docRef);
+        dispatch( activeNote(docRef.id, newNote) )
+    }
+}
+
+export const activeNote = (id, note) => {
+    return {
+        type: types.notesActive,
+        payload: {
+            id,
+            ...note
+        }
     }
 }
